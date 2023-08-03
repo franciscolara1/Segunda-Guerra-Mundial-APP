c265d2b82c352b168b9ecbc8c26f4ee6f33e6536
franciscolara1 <fra.larat@duocuc.cl>
2023-08-03 18:15:18 -0400

    Cuestionario

--- src/app/cuenta/cuenta.page.ts
+++ src/app/cuenta/cuenta.page.ts
@@ -9,8 +9,24 @@ export class CuentaPage  {
 
     show:boolean;
     show2:boolean;
+    show3:boolean;
+    show4:boolean;
+    show5:boolean;
+    show6:boolean;
+    show7:boolean;
+    show8:boolean;
+    show9:boolean;
+    show10:boolean;
     incorrect:boolean;
     incorrect2:boolean;
+    incorrect3:boolean;
+    incorrect4:boolean;
+    incorrect5:boolean;
+    incorrect6:boolean;
+    incorrect7:boolean;
+    incorrect8:boolean;
+    incorrect9:boolean;
+    incorrect10:boolean;
     constructor(){}
 }
 
